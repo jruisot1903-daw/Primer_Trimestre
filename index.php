c89ab83abef2c79db6fe318e147ecec608080a37
jruisot1903-daw <jruisot1903@g.educaand.es>
2025-09-18 12:59:33 +0200

    Estado inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+hola
+<?php 
+    $numero = 15;
+    
+    echo $numero." ";
+
+    $numero ++;
+    $suma = $numero + $numero;
+        if($suma>25)
+            echo  " "."mayor"." ";
+   // phpinfo();
+?>
+adios
