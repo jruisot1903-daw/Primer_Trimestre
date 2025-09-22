724e74326c77c500d532ed58fdddc2b5b7a151e9
jruisot1903-daw <jruisot1903@g.educaand.es>
2025-09-22 14:54:45 +0200

    sintaxis Basica

--- aplicacion/pruebas/sintaxisBasica.php
+++ aplicacion/pruebas/sintaxisBasica.php
@@ -1,7 +1,9 @@
 <?php
 include_once(dirname(__FILE__) . "/../../cabecera.php");
 //Controlador
-
+$var = 12;
+error_reporting(E_ALL); 
+$var++;
 
 //Dibuja la plantilla de la vista 
 inicioCabecera("2DAW APLICACION");
@@ -25,5 +27,9 @@ function cuerpo()
 ?>
     Estas en sintaxisBasica
 <?php
-
+# Necesitamos la etiqueta <br> para hacer el salto de linea visual
+    echo "<br> Esto esta escrito desde PHP".PHP_EOL;#PHP_EOL es un salto de linea
+    echo "<br>Otra linea".PHP_EOL;
+    echo "<br>el host de llamada ".$_SERVER['HTTP_HOST'].
+    " usando el navegador ".$_SERVER['HTTP_USER_AGENT']."<br>".PHP_EOL;
 }
