e82d547d35941fdee8f7ad3ab38852a3c191a989
jruisot1903-daw <jruisot1903@g.educaand.es>
2025-10-09 16:42:19 +0200

    2PracticaTerminada

---
+++ aplicacion/relacion2/ejercicio4/index.php
@@ -0,0 +1,38 @@
+<?php
+include_once(dirname(__FILE__) . "/../../../cabecera.php");
+//Controlador
+
+
+
+//Dibuja la plantilla de la vista 
+inicioCabecera("2DAW APLICACION");
+cabecera();
+finCabecera();
+
+inicioCuerpo("Relacion 2 - Ejercicio 4");
+cuerpo(); //llamo a la vista
+finCuerpo();
+
+
+
+// **********************************************************
+
+//Vista
+function cabecera() {}
+
+//Vista
+function cuerpo()
+{
+   $valor = 17.5;
+   $valor2 =  379987.24;
+
+   $format1 = number_format($valor,1,',','');
+   $format2 = number_format($valor2,2,',','.');
+// STR_PAD_LEFT nos va a servir para poder poner lo 0 o espacios a la izquierda en este caso
+   $valor1_final = '-' . str_pad($format1, 15, '0', STR_PAD_LEFT) . '-';
+   $valor2_final = '-' . str_pad($format2, 15, ' ', STR_PAD_LEFT) . '-';
+
+   echo "Primer valor: $valor1_final <br>";
+   echo "Segundo valor: $valor2_final";
+
+}
---
+++ aplicacion/relacion2/ejercicio5/index.php
@@ -0,0 +1,59 @@
+<?php
+include_once(dirname(__FILE__) . "/../../../cabecera.php");
+//Controlador
+
+
+
+//Dibuja la plantilla de la vista 
+inicioCabecera("2DAW APLICACION");
+cabecera();
+finCabecera();
+
+inicioCuerpo("Relacion 2 - Ejercicio 5");
+cuerpo(); //llamo a la vista
+finCuerpo();
+
+
+
+// **********************************************************
+
+//Vista
+function cabecera() {}
+
+//Vista
+function cuerpo()
+{
+   $texto = <<<HTML
+<p>Este es un párrafo con el número 42</p>
+<div>Otro número: 1234</div>
+<a href="mailto:juan@gmail.com">juan@gmail.com</a>
+<span>Correo alternativo: maria@gmail.org</span>
+<p>Título principal</p>
+HTML;
+
+// Expresiones regulares
+$regex_etiqueta = '/<[^>]+>/';                     // Cualquier etiqueta HTML <...>
+$regex_numero = '/\b\d+\b/';                       // Números (una o varias cifras)
+$regex_email = '/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/'; // Emails válidos
+
+// Buscar coincidencias
+preg_match_all($regex_etiqueta, $texto, $etiquetas, PREG_OFFSET_CAPTURE);
+preg_match_all($regex_numero, $texto, $numeros, PREG_OFFSET_CAPTURE);
+preg_match_all($regex_email, $texto, $emails, PREG_OFFSET_CAPTURE);
+
+echo "<h4>=== ETIQUETAS HTML ===</h4><br>";
+foreach ($etiquetas[0] as $match) {
+    echo "Encontrado '{$match[0]}' en posición {$match[1]}<br>";
+}
+
+echo "<br><h4>=== NÚMEROS ===</h4><br>";
+foreach ($numeros[0] as $match) {
+    echo "Encontrado '{$match[0]}' en posición {$match[1]}<br>";
+}
+
+echo "<br><h4>=== EMAILS ===</h4><br>";
+foreach ($emails[0] as $match) {
+    echo "Encontrado '{$match[0]}' en posición {$match[1]}<br>";
+}
+
+}
